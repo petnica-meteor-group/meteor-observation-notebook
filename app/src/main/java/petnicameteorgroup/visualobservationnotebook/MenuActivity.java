60aca35e2c198a3a0a791ca09e78e020a28b0021
Vladimir Nikolic <vladimir.nik94@gmail.com>
2017-10-28 01:50:28 +0200

    ＡＥＳＴＨＥＴＩＣＳ

--- app/src/main/java/petnicameteorgroup/visualobservationnotebook/MenuActivity.java
+++ app/src/main/java/petnicameteorgroup/visualobservationnotebook/MenuActivity.java
@@ -3,22 +3,25 @@ package petnicameteorgroup.visualobservationnotebook;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     private static int REQUIRED_PERMISSION_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.REORDER_TASKS) != PackageManager.PERMISSION_GRANTED) {
@@ -28,17 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.gif);
         Glide.with(this).load(R.drawable.background).into(imageView);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/ArchitectsDaughter-Regular.ttf");
+        ((TextView) findViewById(R.id.title)).setTypeface(font);
+
+        font = Typeface.createFromAsset(getAssets(), "fonts/JuliusSansOne-Regular.ttf");
+        ((Button) findViewById(R.id.begin_observation_button)).setTypeface(font);
+        ((Button) findViewById(R.id.test_observation_button)).setTypeface(font);
+        ((Button) findViewById(R.id.instructions_button)).setTypeface(font);
+        ((TextView) findViewById(R.id.pmg)).setTypeface(font);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-        }
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {}
     }
 
     public void beginObservation(View v) {
         startActivity(new Intent(this, NotebookActivity.class));
+    }
+
+    public void testObservation(View v) {
+
+    }
+
+    public void showInstructions(View v) {
+
     }
 
 }
